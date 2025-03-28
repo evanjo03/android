d7dec9f4763197806770e4858218b4ad27db3630
John Evans <john.evans@tsi.com>
2025-03-28 16:40:16 -0500

    Updates

--- Sandbox/app/src/main/java/com/example/sandbox/MainActivity.kt
+++ Sandbox/app/src/main/java/com/example/sandbox/MainActivity.kt
@@ -1,41 +1,28 @@
 package com.example.sandbox
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.room.Insert
-import androidx.room.Room
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
 import com.example.sandbox.ui.theme.SandboxTheme
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class MainActivity : ComponentActivity() {
-    private lateinit var db: AppDatabase
-    private lateinit var userDao: UserDao
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        db = Room.databaseBuilder(
-            context = applicationContext,
-            AppDatabase::class.java, "cool-db"
-        ).build()
         setContent {
             SandboxTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
-                        db = db,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -45,16 +32,13 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, db: AppDatabase, modifier: Modifier = Modifier) {
-    val coroutineScope = rememberCoroutineScope()
-
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    Text("Hello, $name")
     Button(onClick = {
-        // Launch a coroutine to perform the database operation off the main thread
-        coroutineScope.launch {
-            withContext(Dispatchers.IO) {
-                db.userDao().insertAll(User(uid = 1, firstName = "John", lastName = "Doe"))
-            }
-        }    }) {
-        Text(text = "Add User")
+        val intent = Intent(context, HelloService::class.java)
+        ContextCompat.startForegroundService(context, intent)
+    }) {
+        Text("Start Service")
     }
 }
